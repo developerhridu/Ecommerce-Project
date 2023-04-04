34c2247c9dde8ac353aa6cd8d3495a5f6eac05cb
“[MR <mizanurrahmanhridu@gmail.com>
2023-04-04 18:26:22 +0600

    Client Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import Home from "../src/pages/user/Home"
-import Login from "../src/pages/auth/login"
 
 const App = () => {
     return (
-       <BrowserRouter>
-           <Toaster />
-           <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/login" element={<Login/>}/>
+        <div>
 
-           </Routes>
-       </BrowserRouter>
+
+        </div>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
+
